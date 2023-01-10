ed06125acf2bc5f2c2c1193dfa459992e96db275
andrey <andreyalgin@yandex.ru>
2023-01-10 16:38:24 +0300

    Test commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#include <unistd.h>
+
+int main(int argc, const char* argv[])
+{
+	std::cout << "Hello" << std::endl;
+
+	return 0;
+}
+
